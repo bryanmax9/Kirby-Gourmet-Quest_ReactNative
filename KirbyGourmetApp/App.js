4c2efbd3633cedd36661214dc2ec79caea4be18c
bryanmax9 <brillant16.gcv@gmail.com>
2024-06-22 17:54:39 -0500

    preparing to use Google API👻📱

--- KirbyGourmetApp/App.js
+++ KirbyGourmetApp/App.js
@@ -12,8 +12,15 @@ import {
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "restaurant",
+  Map: "map",
+  Settings: "settings",
+};
 
 const Settings = () => (
   <AppArea>
@@ -25,6 +32,20 @@ const Map = () => (
     <Text>Map</Text>
   </AppArea>
 );
+
+const screenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarStyle: { backgroundColor: "#F7E8A4" }, // Khaki color for tab bar
+    tabBarActiveTintColor: "#FF69B4", // Bright pink for active tab icon
+    tabBarInactiveTintColor: "#000000", // Black for inactive tab icon
+    headerStyle: { backgroundColor: "#F7E8A4" }, // Khaki color for header
+    headerTitleStyle: { color: "#000000" }, // Black color for header title
+  };
+};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -43,15 +64,7 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={{
-              tabBarStyle: { backgroundColor: "#F7E8A4" }, // Khaki color for tab bar
-              tabBarActiveTintColor: "#FF69B4", // Bright pink for active tab icon
-              tabBarInactiveTintColor: "#000000", // Black for inactive tab icon
-              headerStyle: { backgroundColor: "#F7E8A4" }, // Khaki color for header
-              headerTitleStyle: { color: "#000000" }, // Black color for header title
-            }}
-          >
+          <Tab.Navigator screenOptions={screenOptions}>
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
